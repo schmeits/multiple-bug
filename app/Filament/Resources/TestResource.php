d1ae5a8d2ff72b30bfd7d041330c012ef055d4e2
Tally Schmeits <tally@schmeits.com>
2024-05-27 15:59:30 +0200

    First commit

---
+++ app/Filament/Resources/TestResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TestResource\Pages;
+use App\Filament\Resources\TestResource\RelationManagers;
+use App\Models\Test;
+use Faker\Provider\Text;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TestResource extends Resource
+{
+    protected static ?string $model = Test::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required(),
+
+                Forms\Components\ToggleButtons::make('this_field_does_not_exist')
+                    ->multiple()
+                    ->options([
+                        'tailwind' => 'Tailwind CSS',
+                        'alpine' => 'Alpine.js',
+                        'laravel' => 'Laravel',
+                        'livewire' => 'Laravel Livewire',
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->label('Title'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTests::route('/'),
+            'create' => Pages\CreateTest::route('/create'),
+            'edit' => Pages\EditTest::route('/{record}/edit'),
+        ];
+    }
+}
